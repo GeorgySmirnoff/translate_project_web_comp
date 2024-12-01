ef57a12f76aa6d00baf65b48118822e12242ea0a
Georgy Smirnoff <smirgoff@mail.com>
2024-12-01 17:06:24 +0300

    initial commit

---
+++ precache-manifest.8f4b5722a3225efdb04567c9f39df771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da3a2945f4dd9fd9ca4569934527c53",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "1bf712ac607bcbbd7350",
+    "url": "/dictionary/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/dictionary/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "1bf712ac607bcbbd7350",
+    "url": "/dictionary/static/js/main.4dc9f241.chunk.js"
+  },
+  {
+    "revision": "67d3f92be30e1c42b517",
+    "url": "/dictionary/static/js/runtime~main.dbf1fe99.js"
+  }
+]);
